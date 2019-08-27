2bbf2cd29c083f030afced4fd004b0ef8c1ef69e
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2019-08-27 21:01:05 +0900

    Customize to uwayasu shop

--- src/components/bar.vue
+++ src/components/bar.vue
@@ -5,6 +5,9 @@ import { Bar, mixins } from "vue-chartjs";
 
 @Component
 export default class simpleBar extends Mixins(Bar, mixins.reactiveProp) {
+  // TODO: このプロパティが無いとエラーになるが、使っていないので消したい
+  @Prop({ type: Object, default: null }) chartData!: Chart.ChartData;
+
   @Prop({ type: Object, default: null }) data!: Chart.ChartData;
   @Prop({ type: Object, default: null }) options!: Chart.ChartOptions;
 
@@ -14,4 +17,4 @@ export default class simpleBar extends Mixins(Bar, mixins.reactiveProp) {
 }
 </script>
 
-<style></style>
+<style scoped></style>
